28d3f6104ee62cde5af6951fcea9a7f347afad98
Novashan <novashanm@gmail.com>
2023-02-22 03:21:53 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,23 @@
 
 void times_table(void)
 {
-	int h1, h2;
+	int h1, h2, temp;
 
 	for (h1 = 0; h1 < 10; h1++)
 	{
 		for (h2 = 0; h2 < 10; h2++)
 		{
-			_putchar('0' + (h1 * h2));
+			temp = h1 * h2;
+			if (temp > 9)
+			{
+				_putchar('0' + ((temp - (temp % 10)) / 10);
+				_putchar('0' + (temp % 10));
+			}
+			else
+			{
+				_putchar('0' + temp);
+			}
+
 			if (h2 == 9)
 			{
 				_putchar('\n');
