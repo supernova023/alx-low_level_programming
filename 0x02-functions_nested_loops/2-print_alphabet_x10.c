f7271f54849d879635017082194eba78ccf47ae1
Novashan <novashanm@gmail.com>
2023-02-21 17:53:28 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - print out alphabet
+ *
+ * Return
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
