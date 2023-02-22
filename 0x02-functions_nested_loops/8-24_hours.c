6a551d49aacd27d1a49c3a8d552ae32426a050b1
Novashan <novashanm@gmail.com>
2023-02-21 19:58:28 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * Description - print every minute
+ *
+ * Return
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 < 2; h1++)
+	{
+		for (h2 = 0; h2 < 10; h2++)
+		{
+			for (m1 = 0; m1 < 6; m1++;)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+	for (h1 = 2; h1 < 3; h1++)
+	{
+		for (h2 = 0; h2 < 5; h2++)
+		{
+			for (m1 = 0; m1 < 6; m1++;)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
