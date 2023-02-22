029ce2c7d2d8c936f2d8ba8269d715688c4a7ccf
Novashan <novashanm@gmail.com>
2023-02-22 02:38:07 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * Description - print multiplication table
+ *
+ * Return
+ */
+
+void times_table(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 < 10; h1++)
+	{
+		for (h2 = 0; h2 < 10; h2++)
+		{
+			_putchar('0' + h1 * h2);
+			if (h2 == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
