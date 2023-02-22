314180566be5fbc7704a78e19ff4193e0304fc37
Novashan <novashanm@gmail.com>
2023-02-22 12:59:17 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - determine sum
+ * Return: 0
+ */
+int main(void)
+{
+	int y, sum;
+
+	sum = 0;
+
+	for (y = 1; y < 1024; y++)
+	{
+		if ((y % 3 == 0) || (y % 5 == 0))
+		{
+			sum = sum + y;
+		}
+
+	}
+	printf("%d", sum);
+	return (0);
+}
