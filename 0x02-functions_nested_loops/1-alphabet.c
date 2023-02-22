027ec078dccd3cd9db63f0761a26f189c96f88fc
Novashan <novashanm@gmail.com>
2023-02-21 17:21:01 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print out alphabet
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
