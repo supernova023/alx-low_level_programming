7319db3f3584c29d68e536819f20b8c5be43f72d
Novashan <novashanm@gmail.com>
2023-02-23 19:09:27 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Extra point
+ * Description - multiply 2 numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: num1*num2
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
