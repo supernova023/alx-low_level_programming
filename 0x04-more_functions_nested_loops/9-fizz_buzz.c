047667219dad6737d9d16037987641fd0603d0c4
Novashan <novashanm@gmail.com>
2023-02-24 15:11:02 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,23 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
+		if (x > 1)
+			printf(" ");
 		if (x % 5 == 0 && x % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", x);
+			printf("%d", x);
 		}
 	}
 	printf("\n");
