b8f246fa97a01a4ac52dab2cee33d3cddab2b223
Novashan <novashanm@gmail.com>
2023-02-24 15:21:32 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - Extra point
+ * Description - print triangle size n
+ * @size: variable determines line length
+ *
+ *
+ * Return
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (x = 0; x < size; x++)
+			{
+				if (x < (size - y - 1)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+
+}
