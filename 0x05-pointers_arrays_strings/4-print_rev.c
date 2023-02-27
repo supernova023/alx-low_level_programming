17b70797d5a058879843ab19c2e36fa01b6ba8d2
Novashan <novashanm@gmail.com>
2023-02-27 12:00:11 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - Entry Point
- * Description: output string in reverse 
+ * Description: output string in reverse
  * @s: string to output
  *
  * Return: none
  */
 void print_rev(char *s)
 {
-	int len = -1;
+	int len = 0;
 
 	while (s[len])
 	{
