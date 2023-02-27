c6b3f05471e0a38a705d7155ae0358d91618e13a
Novashan <novashanm@gmail.com>
2023-02-27 09:40:12 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry Point
+ * Description: swap 2 integers
+ * @s: string to find length of
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len;
+	char c[] = *s;
+
+	len = sizeof(c) / sizeof(c[0]);
+	return (len);
+}
