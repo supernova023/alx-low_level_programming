00f9ab31e5c6f3ed634b152adca32d9804054e3c
Novashan <novashanm@gmail.com>
2023-02-27 11:15:17 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Entry Point
+ * Description: output string
+ * @str: string to output
+ *
+ * Return: string length
+ */
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (s[len])
+	{
+		_putchar(str[len]);
+		len++;
+	}
+
+}
