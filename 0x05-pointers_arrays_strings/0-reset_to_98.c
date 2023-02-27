a478ef2ba416986ed3044bd6c9ff9603e997f7c4
Novashan <novashanm@gmail.com>
2023-02-27 05:16:26 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry Point
+ * Description: set to 98
+ * @n: pointer to address of value to be reset to 98.
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
