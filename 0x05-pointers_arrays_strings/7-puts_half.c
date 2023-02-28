f280d21b65800c825c719d74a2e7f88ab05c7c5d
Novashan <novashanm@gmail.com>
2023-02-27 18:43:06 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry Point
+ * Description: output the first half characters of string
+ * @str: string to output
+ *
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+		x  = len / 2;
+	else
+		x = (len -1) / 2;
+
+	len = 0;
+	while (len < x)
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
