5556d9b0ab2bcd35e661c286120b0c3767845e0c
Novashan <novashanm@gmail.com>
2023-03-02 13:45:34 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strncat - Entry point
+ *
+ * @dest: variable storing destination location
+ * @src: variable storing source data
+ * @n: variable limiting number of characters to be used from src
+ * Return: resulting string to destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int cd, i;
+
+	i = 0;
+	cd = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] == '\0')
+		{
+			cd = i;
+			i = n;
+		}
+		else
+		{
+			dest[i] = src[i];
+		}
+	}
+	while (cd < n)
+	{
+		dest[cd] = '\0';
+		cd++;
+	}
+	return (dest);
+}
