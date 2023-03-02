e368d739a9af94ccaf7054a119af902aa7bb9451
Novashan <novashanm@gmail.com>
2023-03-02 10:27:30 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strncat - Entry point
+ *
+ * @dest: variable storing destination location
+ * @src: variable storing source data
+ * @n: variable limiting number of characters to be used from src
+ * Return: resulting string to destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int cd, cs, i;
+
+	i = 0;
+	cd = 0;
+	cs = 0;
+
+	while (dest[cd])
+	{
+		cd++;
+	}
+	while (src[cs])
+	{
+		cs++;
+	}
+	for (i = cd; i < (cd + cs); i++)
+	{
+		if ((i - cd + 1) == n)
+		{
+			break;
+		}
+		else
+		{
+			dest[i] = src[i - cd];
+		}
+	}
+	return (dest);
+}
