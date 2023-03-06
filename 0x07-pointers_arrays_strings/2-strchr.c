8c4c5c9d76c1d896d4bf333370f0d7e51031b2d7
Novashan <novashanm@gmail.com>
2023-03-06 12:02:39 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ *
+ *
+ *@s: pointer to search in for character c
+ *@c: character to search for
+ *Return: position of character or NULL if character not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s += i;
+			return (s);
+		}
+	}
+	return (0);
+}
