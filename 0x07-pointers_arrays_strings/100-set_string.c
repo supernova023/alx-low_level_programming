7b7523b49cadbd93c80281997958d3d260d36dd5
Novashan <novashanm@gmail.com>
2023-03-06 19:45:53 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_string - Entry point
+ *
+ *
+ *@s: string to edit
+ *@to: string to assign
+ *Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
