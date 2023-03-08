5d5bf4a4e292996cd918a21b25ab1785a67be213
Novashan <novashanm@gmail.com>
2023-03-08 15:22:26 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ *
+ * @n: integer to find factorial of
+ * Return: factorial for n greater than or equal to zero, -1 for less than zero
+ */
+
+int factorial(int n)
+{
+	int fac = 1;
+
+	if (n < 0)
+	{
+		fac = -1;
+	}
+	else if (n == 0)
+	{
+		fac = 1;
+	}
+	else
+	{
+		fac = n * factorial(n-1);
+	}
+
+	return (fac);
+}
