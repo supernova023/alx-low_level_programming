7b3ed3a3b14f8d75ea49e760839367205f7eeb7b
Novashan <novashanm@gmail.com>
2023-03-08 17:19:19 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry point
+ *
+ * @n: integer to find natural root
+ * Return: natural root if it exists, -1 if no natural root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int root = 1, x = n;
+
+	if (n < 0)
+	{
+		root = -1;
+	}
+	else if (n == 0)
+	{
+		root = 0;
+	}
+	else
+	{
+		root = sqrtfin(n, x);
+	}
+
+	return (root);
+}
+
+/**
+ * sqrtfin - Entry point
+ * @t: test root
+ * @s: integer to find natural root
+ * Return: natural root if it exists, -1 if no natural root
+ */
+
+int sqrtfin(int s, int t)
+{
+	int r, check = s - (t * t);
+
+	if (t == 0)
+	{
+		return (-1);
+	}
+	if (check == 0)
+	{
+		r = t;
+	}
+	else
+	{
+		r = sqrtfin(s, t - 1);
+	}
+
+	return (r);
+}
