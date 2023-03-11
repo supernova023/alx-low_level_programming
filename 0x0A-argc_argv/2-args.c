2d1987622d8a8ef7c53bf4563f0de34084e212c2
Novashan <novashanm@gmail.com>
2023-03-10 19:52:08 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * @argc: count args
+ * @argv: store all args
+ * Return: exit condition if code is successful or not
+ */
+
+
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
+}
