956a59dd2cb5af981ebf82393f21bc373bdd3808
Novashan <novashanm@gmail.com>
2023-03-14 14:49:20 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - Entry point
+ * @c: The character to enter into array
+ * @size: size of array
+ * Return: array with c of size size.
+ * On error, NULL is returned
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(char));
+	if (a!)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			a[i] = c;
+		}
+	}
+	return (a);
+}
