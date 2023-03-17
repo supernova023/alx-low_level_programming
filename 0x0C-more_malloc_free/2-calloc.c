9fd4aa00d5db5029b0eccb993a044843f34366d9
Novashan <novashanm@gmail.com>
2023-03-16 20:43:35 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * calloc - entry point
+ * @nmemb: number of members
+ * @size: size of memory to allocate
+ *
+ * Return: pointer to allocated memory or NULL
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	l = nmemb * size;
+	p = malloc(l);
+	if (p == NULL)
+		return (NULL);
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
