1b07105db5164909965753f90bd15ffe75c6a12c
Novashan <novashanm@gmail.com>
2023-03-20 19:35:17 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialise variable
+ * @d: variable name for struct
+ *
+ * Return: Void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+		return;
+	}
+
+	if (d->name == 0)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == 0)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+}
