6f6b23f6c311ed5d66f3111fcba408747cee1fbc
Novashan <novashanm@gmail.com>
2023-03-22 13:39:29 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,9 +14,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (action != NULL)
 	{
-		for (i = 0; i < size; i++)
+		if (array != NULL)
 		{
-			(*action)(array[i]);
+			for (i = 0; i < size; i++)
+			{
+				(*action)(array[i]);
+			}
 		}
 	}
 }
