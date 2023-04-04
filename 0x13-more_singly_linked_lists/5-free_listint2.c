8d103fa201a330969b003fc297e668796af4f493
Novashan Moodley <novashanm@gmail.com>
2023-04-04 16:15:08 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free and clear list
+ * @head: list to clear
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *cnod;
+
+	while (*head != NULL)
+	{
+		cnod = *head;
+		*head = (*head)->next;
+		free(cnod);
+	}
+
+}
