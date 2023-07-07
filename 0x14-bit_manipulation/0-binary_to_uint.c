fae90068be4dd36aa0af0e70f2fe327ceb21b5cf
Novashan Moodley <novashanm@gmail.com>
2023-07-06 21:00:01 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-  int c;
-  unsigned int uint = 0;
+	int c;
+	unsigned int uint = 0;
 
-  if (!b)
-    {
-      return (0);
-    }
+	if (!b)
+	{
+		return (0);
+	}
 
-  for (c = 0; b[c]; c++)
-    {
-      if (b[c] < 48 || b[c] > 49)
-	return (0);
+	for (c = 0; b[c]; c++)
+	{
+		if (b[c] < 48 || b[c] > 49)
+			return (0);
 
-      uint = 2 * uint + (b[c] - 48);
-    }
-  return (uint);
+		uint = 2 * uint + (b[c] - 48);
+	}
+	return (uint);
 }
