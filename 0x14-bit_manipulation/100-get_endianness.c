9917deff02eedeade7cc55ac227d06e5de480baf
Novashan Moodley <novashanm@gmail.com>
2023-08-03 14:46:06 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - check to see if computer is storing in big or little
+ * endianness format
+ *
+ * Return: 1 for little endianness and 0 for big endianness
+ */
+
+int get_endianness(void)
+{
+	unsigned int endianness = 1;
+
+	char *check = (char*) &endianness;
+	return (int)(*check);
+
+}
